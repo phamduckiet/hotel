1e98b2489961b28308641f1ab4682d6c1dd5918b
phamduckiet <duckietpham@gmail.com>
2023-10-03 00:28:29 +0700

    database

---
+++ database/migrations/2023_10_02_172327_create_bookings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->timestamp('checkin');
+            $table->timestamp('checkout');
+            $table->unsignedBigInteger('room_type_id');
+            $table->unsignedBigInteger('room_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
