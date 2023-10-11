7c64d0a430e437ef7e6d68ec9510dec8e8e256b4
Vu Minh Hieu <hieuvm@topcv.vn>
2023-10-11 22:43:53 +0700

    Add roles and permissions p2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
